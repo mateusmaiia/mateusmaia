6a0f6344c1f2bc529152b526aae2d9cdad1ed21a
Maia <matteusmaia889@gmail.com>
2024-06-12 12:17:53 -0300

    feat: Sonner Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import { Toaster } from 'sonner';
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
+        <Toaster richColors/>
       </body>
     </html>
   );
