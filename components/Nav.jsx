438d0f7807d0b19ff050f187026affe7e19c09bd
Maia <matteusmaia889@gmail.com>
2024-06-12 12:16:35 -0300

    update: omite services route

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,10 +8,10 @@ const links = [
     name: "home",
     path: "/",
   },
-  {
-    name: "services",
-    path: "/services",
-  },
+  // {
+  //   name: "services",
+  //   path: "/services",
+  // },
   {
     name: "resume",
     path: "/resume",
