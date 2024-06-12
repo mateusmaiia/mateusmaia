971f526edef46b3341c56881e9193d7e27dfcc62
Maia <matteusmaia889@gmail.com>
2024-06-12 12:17:24 -0300

    feat: using zod, sonner and react-hook-form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,29 +3,46 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectLabel,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import { Irish_Grover } from "next/font/google";
+import { toast } from "sonner";
+
+
+const ContactSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  textarea: z.string(),
+  phoneNumber: z.string().regex(/^\(?\d{2}\)?[\s-]?\d{4,5}[\s-]?\d{4}$/, {
+    message: "Invalid Brazilian phone number format",
+  })
+})
 
 const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
-    description: "(+40) 321 654 876",
+    description: "(+83) 99953-3314",
   },
   {
     icon: <FaEnvelope />,
     title: "Email",
-    description: "youremail@gmail.com",
+    description: "mateusssmaia@gmail.com",
   },
   {
     icon: <FaMapMarkerAlt />,
@@ -34,15 +51,36 @@ const info = [
   },
 ];
 
-import { motion } from "framer-motion";
-
 const Contact = () => {
+
+  const {handleSubmit, register, reset, formState: {isSubmitSuccessful}} = useForm({
+    resolver: zodResolver(ContactSchema),
+  })
+
+  function handleSendEmail(data){
+   try {
+      if(isSubmitSuccessful) {
+        reset({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phoneNumber: "",
+          textarea: "",
+        });
+      }
+      toast.success('Email enviado com sucesso');
+      return console.log(data)
+   } catch (error) {
+    
+   }
+
+  }
   return (
     <motion.section
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        transition: { delay: 1, duration: 0.4, ease: "easeIn" },
       }}
       className="py-6"
     >
@@ -50,21 +88,21 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={handleSubmit(handleSendEmail)}>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
-                nihil sapiente pariatur id totam.
+              Focused on delivering top-notch Web Development services, we transform your ideas into functional and beautiful websites. Contact us today to see how we can help you succeed online.
               </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email address" />
-                <Input type="phone" placeholder="Phone number" />
+                <Input type="firstname" placeholder="firstName" {...register("firstName")} />
+                <Input type="lastname" placeholder="lastName" {...register("lastName")}/>
+                <Input type="email" placeholder="emailAddress"  {...register("email")}/>
+                <Input type="phone" placeholder="phoneNumber" {...register("phoneNumber")}/>
               </div>
-              {/* select */}
-              <Select>
+              {/*
+              select
+               <Select>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
@@ -76,14 +114,15 @@ const Contact = () => {
                     <SelectItem value="mst">Logo Design</SelectItem>
                   </SelectGroup>
                 </SelectContent>
-              </Select>
+              </Select> */}
               {/* textarea */}
               <Textarea
                 className="h-[200px]"
                 placeholder="Type your message here."
+                {...register("textarea")}
               />
               {/* btn */}
-              <Button size="md" className="max-w-40">
+              <Button type="submit" size="md" className="max-w-40">
                 Send message
               </Button>
             </form>
