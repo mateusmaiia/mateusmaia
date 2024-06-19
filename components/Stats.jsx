a96f6d3cd5399190334199072ea102038d588bbe
Maia <matteusmaia889@gmail.com>
2024-06-19 12:10:34 -0300

    update : stats

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -4,19 +4,19 @@ import CountUp from "react-countup";
 
 const stats = [
   {
-    num: 12,
+    num: +2,
     text: "Years of experience",
   },
   {
-    num: 26,
+    num: 34,
     text: "Projects completed",
   },
   {
-    num: 8,
+    num: 12,
     text: "Technologies mastered",
   },
   {
-    num: 500,
+    num: 995,
     text: "Code commits",
   },
 ];
