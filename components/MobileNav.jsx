2a65b4510fb0ff4bdacfde14082e0e478e2566c8
Maia <matteusmaia889@gmail.com>
2024-06-19 12:09:28 -0300

    update: re placed  sheet with tabs

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation"
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const links = [
   {
     name: "home",
     path: "/",
+    // icon: <CiMenuFries className="text-[32px] text-accent" />
   },
   // {
   //   name: "services",
@@ -17,53 +18,58 @@ const links = [
   {
     name: "resume",
     path: "/resume",
+    // icon: <CiMenuFries className="text-[32px] text-accent" />
   },
   {
     name: "work",
     path: "/work",
+    // icon: <CiMenuFries className="text-[32px] text-accent" />
   },
   {
     name: "contact",
     path: "/contact",
+    // icon: <CiMenuFries className="text-[32px] text-accent" />
   },
 ];
 
 const MobileNav = () => {
   const pathname = usePathname();
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-accent" />
-      </SheetTrigger>
-      <SheetContent className="flex flex-col">
-        {/* logo */}
-        <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
-            <h1 className="text-4xl font-semibold">
-              Maia<span className="text-accent">.</span>
-            </h1>
-          </Link>
-        </div>
-        {/* nav */}
-        <nav className="flex flex-col justify-center items-center gap-8">
-          {links.map((link, index) => {
-            return (
+    <Tabs className="w-full bg-primary mt-10 ">
+      <TabsList  className="w-full  m-0 p-4 border-t bg-primary rounded-none bg-transparent rounded-0  border-gray-700 ">
+        {links.map((link, index) => {
+          return(
+            <TabsTrigger  key={index} className="bg-primary  flex-grow w-full rounded-none">
               <Link
-                href={link.path}
-                key={index}
-                className={`${
-                  link.path === pathname &&
-                  "text-accent border-b-2 border-accent"
-                } text-xl capitalize hover:text-accent transition-all`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
-        </nav>
-      </SheetContent>
-    </Sheet>
+              href={link.path}
+              className={`${
+                link.path === pathname &&
+                "text-accent-hover border-b-2 border-accent-hover "
+                } text-xl text-white capitalize transition-all `}
+                >
+              {link.name}
+            </Link>
+            </TabsTrigger>
+          ) 
+        })}
+        </TabsList>
+    </Tabs>
   );
 };
 
 export default MobileNav;
+
+// links.map((link, index) => {
+//   return (
+//     <Link
+//       href={link.path}
+//       key={index}
+//       className={`${
+//         link.path === pathname &&
+//         "text-accent border-b-2 border-accent"
+//       } text-xl capitalize hover:text-accent transition-all`}
+//     >
+//       {link.name}
+//     </Link>
+//   );
+// })}
