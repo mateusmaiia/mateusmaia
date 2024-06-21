aa54855e201ab47b62208de1d79f677ff4e50e73
maia <matteusmaia889@gmail.com>
2024-06-21 14:41:51 -0300

    update: to english

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -120,9 +120,9 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={handleSubmit(handleSendEmail)}>
-              <h3 className="text-4xl text-accent">Vamos trabalhar juntos!</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together!</h3>
               <p className="text-white/60">
-                Focado em fornecer serviços de Desenvolvimento Web de alto nível, transformamos suas ideias em sites funcionais e bonitos. Contate-nos hoje para ver como podemos ajudá-lo a ter sucesso online.
+                Focused on providing high-level Web Development services, we transform your ideas into functional and beautiful websites. Contact us today to see how we can help you succeed online.
               </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
